00bc8094f5455ab08b4810282f196b494ed1bac5
Soumo Sarkar <soumosarkar297@gmail.com>
2026-06-24 22:51:45 +0530

    fix: remove manifest file setting in build script for Windows resources

--- echoinput/build.rs
+++ echoinput/build.rs
@@ -4,7 +4,6 @@ fn main() {
         let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
         let mut res = winresource::WindowsResource::new();
         res.set_icon(&format!("{}/../assets/favicon.ico", manifest_dir));
-        res.set_manifest_file(&format!("{}/resources/manifest.xml", manifest_dir));
         res.compile().expect("failed to compile Windows resources");
     }
 }
