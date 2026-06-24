f7e15eb216de3aa2086702903f7d9f2f0f46ccf6
Soumo Sarkar <soumosarkar297@gmail.com>
2026-06-24 20:38:22 +0530

    fix: update resource paths in build script to use manifest directory

--- echoinput/build.rs
+++ echoinput/build.rs
@@ -1,9 +1,10 @@
 fn main() {
     #[cfg(target_os = "windows")]
     {
+        let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
         let mut res = winresource::WindowsResource::new();
-        res.set_icon("../../assets/favicon.ico");
-        res.set_manifest_file("resources/manifest.xml");
+        res.set_icon(format!("{}/../../assets/favicon.ico", manifest_dir));
+        res.set_manifest_file(format!("{}/resources/manifest.xml", manifest_dir));
         res.compile().expect("failed to compile Windows resources");
     }
 }
