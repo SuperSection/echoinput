0e69c865bd5ff3bd9af9b017e7910b06c6fe4deb
Soumo Sarkar <soumosarkar297@gmail.com>
2026-06-24 20:29:33 +0530

    fix: update resource handling for Windows, replacing embed-resource with winresource

--- echoinput/build.rs
+++ echoinput/build.rs
@@ -1,10 +1,9 @@
 fn main() {
     #[cfg(target_os = "windows")]
     {
-        println!("cargo:rerun-if-changed=resources/echoinput.rc");
-        println!("cargo:rerun-if-changed=resources/manifest.xml");
-        println!("cargo:rerun-if-changed=../../assets/favicon.ico");
-
-        embed_resource::compile("resources/echoinput.rc", [] as [&str; 0]);
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("../../assets/favicon.ico");
+        res.set_manifest_file("resources/manifest.xml");
+        res.compile().expect("failed to compile Windows resources");
     }
 }
